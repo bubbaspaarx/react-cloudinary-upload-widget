975a687528e09133c469f01c5d60fb3ebbc25569
Edward Sparks <edd_sparks@hotmail.com>
2020-11-24 18:33:43 +0000

    axios options fixed

--- src/functions/getterFunction.js
+++ src/functions/getterFunction.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-axios.interceptors.response.use((response) => response.data)
+const instance = axios.create()
+
+instance.interceptors.response.use((response) => response.data)
 
 export const getterFunction = ({
   url,
@@ -9,14 +11,14 @@ export const getterFunction = ({
   contentType,
   withCredentials
 }) => {
-  axios.defaults.headers.common['Accepts'] = accepts
-  axios.defaults.headers.common['Content-Type'] = contentType
-  axios.defaults.withCredentials = withCredentials
+  instance.defaults.headers.common['Accepts'] = accepts
+  instance.defaults.headers.common['Content-Type'] = contentType
+  instance.defaults.withCredentials = withCredentials
   const options = {
     url: url + '?nocache=' + new Date().getTime(),
     method: 'get',
     params: data
   }
 
-  return axios(options)
+  return instance(options)
 }
