d56661e7be6225a2ded7767846393ca64249e08b
Edward Sparks <edd_sparks@hotmail.com>
2020-11-24 16:13:01 +0000

    small change to add resource type to request

--- example/src/App.js
+++ example/src/App.js
@@ -5,58 +5,28 @@ const App = () => (
   <>
     <WidgetLoader />
     <Widget
-      // sources={['local', 'camera']}
-      // // resourceType={'image'} // optionally set with 'auto', 'image', 'video' or 'raw' -> default = 'auto'
-      // cloudName={'goatagency'} // your cloudinary account cloud name. Located on https://cloudinary.com/console/
-      // uploadPreset={'preset1'} // check that an upload preset exists and check mode is signed or unisgned
-      // buttonText={'Open'} // default 'Upload Files'
-      // style={null} // inline styling only or style id='cloudinary_upload_button'
+      sources={['local', 'camera']}
+      resourceType={'auto'} // optionally set with 'auto', 'image', 'video' or 'raw' -> default = 'auto'
+      cloudName={'goatagency'} // your cloudinary account cloud name. Located on https://cloudinary.com/console/
+      uploadPreset={'preset1'} // check that an upload preset exists and check mode is signed or unisgned
+      buttonText={'Open'} // default 'Upload Files'
+      style={null} // inline styling only or style id='cloudinary_upload_button'
       // generateSignatureUrl={
       //   'http://localhost:3000/api/v1/contents/generate_signature'
       // } // e.g. 'http://my_domain.com/api/v1/media/generate_signature' -> check cloudinary docs for signing uploads
-      // // apiKey={'333719262292942'} // cloudinary API key -> number format
-      // // // folder // set cloudinary folder name to send file
-      // // cropping={false} // set ability to crop images -> default = true
-      // // onSuccess // add success callback -> returns result
-      // // onFailure // add failure callback -> returns 'response.error' + 'response.result'
-      // // logging={false} // default = true
-      // // imageName // set a specific image name
-      // // eager // e.g. 'w_400,h_300,c_pad|w_260,h_200,c_crop'
-      // // accepts // for signed uploads only -> default = 'application/json'
-      // // contentType // for signed uploads only -> default = 'application/json'
-      // // withCredentials={true} // default = true -> check axios documentation for more information
-      // // use_filename={true} // tell Cloudinary to use the original name of the uploaded file as its public ID -> default = true,
-      // // unique_filename={true} // setting it to false, you can tell Cloudinary not to attempt to make the Public ID unique, and just use the normalized file name -> default = true
-      // apiKey={'333719262292942'}
-      // accepts={'application/json'}
-      // contentType={'application/json'}
-      // withCredentials={true}
-      // unique_filename={true}
-      // resourceType={'image'}
-      sources={['local']}
-      cloudName={'goatagency'}
-      uploadPreset={'goatagency1'}
-      buttonText={'Open'}
-      style={{
-        color: 'white',
-        border: 'none',
-        width: '120px',
-        backgroundColor: 'green',
-        borderRadius: '4px',
-        height: '25px'
-      }}
-      onSuccess={(res) => console.log(res)}
-      onFailure={(res) => console.log(res)}
-      logging={true}
-      generateSignatureUrl={
-        'http://localhost:3000/api/v1/contents/generate_signature'
-      }
-      apiKey={'333719262292942'}
-      accepts={'application/json'}
-      contentType={'application/json'}
-      withCredentials={true}
-      unique_filename={true}
-      resourceType={'image'}
+      // apiKey={'???????????'} // cloudinary API key -> number format
+      // // folder // set cloudinary folder name to send file
+      // cropping={false} // set ability to crop images -> default = true
+      // onSuccess // add success callback -> returns result
+      // onFailure // add failure callback -> returns 'response.error' + 'response.result'
+      // logging={false} // default = true
+      // imageName // set a specific image name
+      // eager // e.g. 'w_400,h_300,c_pad|w_260,h_200,c_crop'
+      // accepts // for signed uploads only -> default = 'application/json'
+      // contentType // for signed uploads only -> default = 'application/json'
+      // withCredentials // default = true -> check axios documentation for more information
+      // use_filename // tell Cloudinary to use the original name of the uploaded file as its public ID -> default = true,
+      // unique_filename // setting it to false, you can tell Cloudinary not to attempt to make the Public ID unique, and just use the normalized file name -> default = true
     />
   </>
 )
