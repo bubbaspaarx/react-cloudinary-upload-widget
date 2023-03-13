a8db9c0f252302b354db5ff86ec80db728938b4e
Edward Sparks <edd_sparks@hotmail.com>
2023-03-13 13:20:28 +0000

    reverting to 2.0 url

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import UploadWidget from './components/UploadWidget'
 import useScript from './_hooks_/useScript'
 
 export const WidgetLoader = () => (
-  <>{useScript('https://upload-widget.cloudinary.com/global/all.js')}</>
+  <>{useScript('https://widget.cloudinary.com/v2.0/global/all.js')}</>
 )
 export const Widget = (props) => <UploadWidget {...props} />
